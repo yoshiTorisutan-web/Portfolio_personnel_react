e6566043df15dc84d74540e32b76ad6738505803
yoshiTorisutan-web <creation_tristoune_1@outlook.fr>
2024-08-19 22:56:51 +0200

    maj footer logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,13 +6,31 @@ function Footer() {
   return (
     <footer className="footer flex flex-col md:flex-row justify-between items-center bg-orange-500 p-4 md:p-8">
       <div className="footer__left flex items-center">
-        <img
-          src={logoTransparent}
-          alt="Logo"
-          className="footer__logo w-12 h-12"
-        />
-        <Link className="footer__text text-white text-[10px] md:text-[16px] font-poppins" to="/mentions-legales">Mentions Légales</Link>
-        <Link className="footer__text text-white text-[10px] md:text-[16px] font-poppins ml-4" to="/confidential-politic">Politique de confidentialité</Link>
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img
+            src={logoTransparent}
+            alt="Logo"
+            className="footer__logo w-12 h-12"
+          />
+        </Link>
+        <Link
+          className="footer__text text-white text-[10px] md:text-[16px] font-poppins"
+          to="/mentions-legales"
+        >
+          Mentions Légales
+        </Link>
+        <Link
+          className="footer__text text-white text-[10px] md:text-[16px] font-poppins ml-4"
+          to="/confidential-politic"
+        >
+          Politique de confidentialité
+        </Link>
       </div>
       <div className="footer__right text-right mt-4 md:mt-0">
         <p className="footer__text text-white text-[10px] md:text-[16px] font-poppins">
