fe7c4f0b9518feafbd85d1010c5229efd99267af
yoshiTorisutan-web <creation_tristoune_1@outlook.fr>
2024-10-02 00:20:32 +0200

    maj security

---
+++ src/components/config.js
@@ -0,0 +1,13 @@
+// src/config.js
+const config = {
+  emailJS: {
+    serviceID: "service_g6go4si",
+    templateID: "template_cs4lein",
+    userID: "Jl5HetIi5YSumILmw",
+  },
+  reCAPTCHA: {
+    siteKey: "6Ldg51QqAAAAAJFURQrap4vkUExCBnr9fWWsJWmW",
+  },
+};
+
+export default config;
