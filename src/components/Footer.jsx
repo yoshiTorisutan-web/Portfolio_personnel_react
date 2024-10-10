2db7d888d9be4bbb6cbce6c9f09d37609b12f633
yoshiTorisutan-web <creation_tristoune_1@outlook.fr>
2024-10-10 23:33:00 +0200

    maj footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import "../index.css";
 
 function Footer() {
   return (
-    <footer className="footer flex flex-col md:flex-row justify-center md:justify-between items-center bg-orange-500 p-4 md:p-8">
-      <div className="footer__left flex flex-col md:flex-row items-center md:items-center text-center md:text-left">
+    <footer className="footer flex flex-col md:flex-row justify-between items-center bg-orange-500 p-4 md:p-8">
+      <div className="footer__left flex items-center">
         <Link
           to="/"
           className="flex items-center gap-2"
@@ -20,31 +20,23 @@ function Footer() {
           />
         </Link>
         <Link
-          className="footer__text footer__hyperlien text-white text-[10px] md:text-[16px] font-poppins mt-2 md:mt-0 md:ml-4"
+          className="footer__text footer__hyperlien text-white text-[10px] md:text-[16px] font-poppins"
           to="/mentions-legales"
-          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+          onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}
         >
           Mentions légales
         </Link>
         <Link
-          className="footer__text footer__hyperlien text-white text-[10px] md:text-[16px] font-poppins mt-2 md:mt-0 md:ml-4"
+          className="footer__text footer__hyperlien text-white text-[10px] md:text-[16px] font-poppins ml-4"
           to="/confidential-politic"
-          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+          onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}
         >
           Politique de confidentialité
         </Link>
       </div>
-      <div className="footer__right text-center md:text-right mt-4 md:mt-0">
+      <div className="footer__right text-right mt-4 md:mt-0">
         <p className="footer__text text-white text-[10px] md:text-[16px] font-poppins">
-          Copyright © 2024 - Créé et développé par{" "}
-          <b>
-            <a
-              className="footer__hyperlien"
-              href="https://www.linkedin.com/in/tristan-bossard-dw/"
-            >
-              Tristan Bossard
-            </a>
-          </b>
+          Copyright © 2024 - Créé et développé par <b><a className="footer__hyperlien" href="https://www.linkedin.com/in/tristan-bossard-dw/">Tristan Bossard</a></b>
         </p>
       </div>
     </footer>
