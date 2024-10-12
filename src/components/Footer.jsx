792d11c71ba81d0ffe3eae5719c4f2611189d172
yoshiTorisutan-web <creation_tristoune_1@outlook.fr>
2024-10-12 14:04:24 +0200

    maj footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,39 +4,46 @@ import "../index.css";
 
 function Footer() {
   return (
-    <footer className="footer flex flex-col md:flex-row justify-between items-center bg-orange-500 p-4 md:p-8">
-      <div className="footer__left flex items-center">
+    <footer className="footer flex flex-col md:flex-row justify-between items-center p-4 md:p-8">
+      {/* Logo et Liens */}
+      <div className="footer__left flex flex-col md:flex-row items-center gap-4">
         <Link
           to="/"
           className="flex items-center gap-2"
-          onClick={() => {
-            window.scrollTo(0, 0);
-          }}
+          onClick={() => window.scrollTo(0, 0)}
         >
-          <img
-            src={logoTransparent}
-            alt="Logo"
-            className="footer__logo w-12 h-12"
-          />
+          <img src={logoTransparent} alt="Logo" className="w-20 h-20" />
         </Link>
-        <Link
-          className="footer__text footer__hyperlien text-white text-[10px] md:text-[16px] font-poppins"
-          to="/mentions-legales"
-          onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}
-        >
-          Mentions légales
-        </Link>
-        <Link
-          className="footer__text footer__hyperlien text-white text-[10px] md:text-[16px] font-poppins ml-4"
-          to="/confidential-politic"
-          onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}
-        >
-          Politique de confidentialité
-        </Link>
+        <div className="flex flex-col md:flex-row items-center gap-4">
+          <Link
+            className="text-white text-[10px] md:text-[16px] font-poppins footer__hyperlien"
+            to="/mentions-legales"
+            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+          >
+            Mentions légales
+          </Link>
+          <Link
+            className="text-white text-[10px] md:text-[16px] font-poppins footer__hyperlien"
+            to="/confidential-politic"
+            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+          >
+            Politique de confidentialité
+          </Link>
+        </div>
       </div>
-      <div className="footer__right text-right mt-4 md:mt-0">
-        <p className="footer__text text-white text-[10px] md:text-[16px] font-poppins">
-          Copyright © 2024 - Créé et développé par <b><a className="footer__hyperlien" href="https://www.linkedin.com/in/tristan-bossard-dw/">Tristan Bossard</a></b>
+
+      {/* Copyright */}
+      <div className="text-center md:text-right mt-4 md:mt-0">
+        <p className="text-white text-[10px] md:text-[16px] font-poppins">
+          Copyright © 2024 - Créé et développé par{" "}
+          <b>
+            <a
+              className="footer__hyperlien"
+              href="https://www.linkedin.com/in/tristan-bossard-dw/"
+            >
+              Tristan Bossard
+            </a>
+          </b>
         </p>
       </div>
     </footer>
