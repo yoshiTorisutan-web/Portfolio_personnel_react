0b55a3a68d2fd92ed00250572cf1cbd329c74045
yoshiTorisutan-web <creation_tristoune_1@outlook.fr>
2025-10-19 14:41:09 +0200

    update tech

--- src/constants/index.js
+++ src/constants/index.js
@@ -109,42 +109,42 @@ const hobbies = [
 const technologies = [
   {
     name: "HTML",
-    temps: "5 ans",
+    temps: "6 ans",
     icon: html,
   },
   {
     name: "CSS",
-    temps: "5 ans",
+    temps: "6 ans",
     icon: css,
   },
   {
     name: "JavaScript",
-    temps: "5 ans",
+    temps: "6 ans",
     icon: javascript,
   },
   {
     name: "React",
-    temps: "3 ans",
+    temps: "4 ans",
     icon: reactjs,
   },
   {
     name: "Tailwind",
-    temps: "3 ans",
+    temps: "4 ans",
     icon: tailwind,
   },
   {
     name: "Node",
-    temps: "3 ans",
+    temps: "4 ans",
     icon: nodejs,
   },
   {
     name: "git",
-    temps: "5 ans",
+    temps: "6 ans",
     icon: git,
   },
   {
     name: "figma",
-    temps: "5 ans",
+    temps: "6 ans",
     icon: figma,
   },
   {
@@ -169,12 +169,12 @@ const technologies = [
   },
   {
     name: "mysql",
-    temps: "4 ans",
+    temps: "5 ans",
     icon: mysql,
   },
   {
     name: "php",
-    temps: "5 ans",
+    temps: "6 ans",
     icon: php,
   },
 ];
